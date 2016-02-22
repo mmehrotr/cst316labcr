ad1973bc42c7eed1a37a668e368c262c4fc7d8ac
mmehrotr <mmehrotr@asu.edu>
2016-02-22 15:14:40 -0700

    Fixed the banner comment for main

--- given/src/banking/gui/Main.java
+++ given/src/banking/gui/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 /**
  * main method for running the program.
  * @author kevinagary
+ * @date 2/22/16
  *
  */
 final class Main {
@@ -18,6 +19,9 @@ final class Main {
 	 * All methods should have a Javadoc according to STYLE.
 	 * @param args command-line arguments
 	 * @throws Exception as per typical main specifications
+	 * @returns null
+	 * 
+	 * @Description is the main entry point for the program
 	 */
 	public static void main(final String[] args) throws Exception {
 
